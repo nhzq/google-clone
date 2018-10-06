cb7d4d0b4a6ea8b8ef7648974a3a03eb9d740987
noorhaziq <code.noorhaziq@gmail.com>
2018-10-06 11:33:54 +0800

    4. Creating our index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+    <div class="main-section">
+        <div class="logo-container">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+        </div>
+
+        <div class="search-container">
+            <input type="text" name="term" class="search-box">
+            <input type="submit" value="Search" class="search-button">
+        </div>
+    </div>
+</body>
+</html>
