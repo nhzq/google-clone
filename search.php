7388acfebc34edf5386e91093f740939812b6980
noorhaziq <code.noorhaziq@gmail.com>
2018-10-07 16:52:45 +0800

    11. Creating our search page structure

--- search.php
+++ search.php
@@ -1,3 +1,32 @@
-<?php  
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Doodle</title>
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="header-content">
+                <div class="logo-container">
+                    <a href="index.php">
+                        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+                    </a>
+                </div>
+
+                <div class="search-container">
+                    <form action="search.php" method="GET">
+                        <div class="search-bar-container">
+                            <input class="search-box" type="text" name="term">
+                            <button>
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
