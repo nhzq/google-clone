6cc23e70dc61442b11b4717520615a834566d446
noorhaziq <code.noorhaziq@gmail.com>
2018-10-07 16:00:10 +0800

    7&8. Styling index and search button

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>
-    <div class="main-section">
-        <div class="logo-container">
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-        </div>
+    <div class="wrapper index-page">
+        <div class="main-section">
+            <div class="logo-container">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+            </div>
 
-        <div class="search-container">
-            <form action="search.php" method="GET">
-                <input type="text" name="term" class="search-box">
-                <input type="submit" value="Search" class="search-button">
-            </form>
+            <div class="search-container">
+                <form action="search.php" method="GET">
+                    <input type="text" name="term" class="search-box">
+                    <input type="submit" value="Search" class="search-button">
+                </form>
+            </div>
         </div>
     </div>
 </body>
